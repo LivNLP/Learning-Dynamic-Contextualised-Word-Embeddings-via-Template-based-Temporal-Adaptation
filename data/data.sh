e911fa9e5751054f3eff29cdaeb4422c3a8cf198
xiaohang-tang <52322566+xiaohang-tang@users.noreply.github.com>
2023-05-13 16:09:39 +0100

    Add files via upload

---
+++ data/data.sh
@@ -0,0 +1,4 @@
+wget -P ./data https://files.pushshift.io/reddit/comments/RC_2019-09.zst
+wget -P ./data https://files.pushshift.io/reddit/comments/RC_2020-04.zst
+wget -P ./data https://www.kaggle.com/datasets/Cornell-University/arxiv/download?datasetVersionNumber=99
+wget -P ./data https://www.cse.msu.edu/~tangjili/Ciao.rar
